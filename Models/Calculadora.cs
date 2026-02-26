1601e49771c8546e135720151dbf66eb13e866f2
DevJav <random11112005@gmail.com>
2026-02-26 16:33:53 -0400

    Agregando carpeta Models

---
+++ Models/Calculadora.cs
@@ -0,0 +1,18 @@
+using System; 
+ 
+namespace GeoApp.Models 
+{ 
+    public static class Calculadora 
+    { 
+        //Rectangulo
+        public static double AreaRectangulo(double baseR, double altura) => baseR * altura; 
+
+        //Circulo
+
+
+        //Triangulo
+
+
+
+    } 
+} 
