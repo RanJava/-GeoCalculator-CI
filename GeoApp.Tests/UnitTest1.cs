9f9d7940bd49eb9593fce404de4df096e19f31b7
DevJav <random11112005@gmail.com>
2026-02-26 22:31:14 -0400

    Pipeline

---
+++ GeoApp.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Xunit; 
+using GeoApp.Models; 
+
+public class UnitTest1 
+{ 
+    [Fact] 
+    public void TestAreaRectangulo() 
+    { 
+        double resultado = Calculadora.AreaRectangulo(10, 5); 
+        Assert.Equal(50, resultado); 
+    } 
+
+    [Fact] 
+    public void TestAreaCirculo() 
+    { 
+        double resultado = Calculadora.AreaCirculo(7); 
+        Assert.Equal(Math.PI * 49, resultado); 
+    } 
+
+    [Fact] 
+    public void TestAreaTriangulo() 
+    { 
+        double resultado = Calculadora.AreaTriangulo(10, 5); 
+        Assert.Equal(25, resultado); 
+    } 
+}
