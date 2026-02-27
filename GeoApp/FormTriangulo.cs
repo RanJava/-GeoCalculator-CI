ce9bb188eda6d60af36c81678900f8d9a50c5afe
DevJav <random11112005@gmail.com>
2026-02-26 22:10:55 -0400

    Correcion de estilo

--- GeoApp/FormTriangulo.cs
+++ GeoApp/FormTriangulo.cs
@@ -1,43 +1,37 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 using GeoApp.Models; 
+
 namespace GeoApp
 {
     public partial class FormTriangulo : Form
     {
-        private TextBox txtBase;
-        private TextBox txtAltura;
-        private Label lblResultado;
+        private TextBox txtBase = new TextBox { Location = new Point(100, 20), Size = new Size(150, 25) };
+        private TextBox txtAltura = new TextBox { Location = new Point(100, 60), Size = new Size(150, 25) };
+        private Button btnCalcular = new Button { Text = "Calcular", Location = new Point(100, 100), Size = new Size(90, 30) };
 
         public FormTriangulo()
         {
-            this.Text = "Calculo de Triangulo ";
-            this.Size = new System.Drawing.Size(300, 300);
+            this.Text = "Cálculo de Triángulo";
+            this.Size = new Size(320, 200);
 
-            // Crear controles
-            Label lblBase = new Label { Text = "Base:", Location = new System.Drawing.Point(20, 20) };
-            txtBase = new TextBox { Location = new System.Drawing.Point(20, 45), Width = 100 };
-            
-            Label lblAlt = new Label { Text = "Altura:", Location = new System.Drawing.Point(20, 80) };
-            txtAltura = new TextBox { Location = new System.Drawing.Point(20, 105), Width = 100 };
+            this.Controls.Add(new Label { Text = "Base:", Location = new Point(20, 23), AutoSize = true });
+            this.Controls.Add(txtBase);
+            this.Controls.Add(new Label { Text = "Altura:", Location = new Point(20, 63), AutoSize = true });
+            this.Controls.Add(txtAltura);
+            this.Controls.Add(btnCalcular);
 
-            Button btnCalcular = new Button { Text = "Calcular", Location = new System.Drawing.Point(20, 150) };
-            lblResultado = new Label { Text = "Resultado: ", Location = new System.Drawing.Point(20, 190), Width = 200 };
-
-            // Evento del botón
             btnCalcular.Click += (sender, e) => {
                 try {
                     double b = double.Parse(txtBase.Text);
                     double h = double.Parse(txtAltura.Text);
                     double res = Calculadora.AreaTriangulo(b, h);
-                    lblResultado.Text = $"Area: {res} u²";
+                    MessageBox.Show("El área es: " + res);
                 } catch {
                     MessageBox.Show("Ingresa números válidos");
                 }
             };
-
-            this.Controls.AddRange(new Control[] { lblBase, txtBase, lblAlt, txtAltura, btnCalcular, lblResultado });
         }
-        
     }
 }
